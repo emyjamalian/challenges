76fc8dafd0fed10f375dfcc9e41ed25ad359f57a
emyjamalian <emy.jamalian@gmail.com>
2023-11-02 11:38:15 +0100

    challenge 2: meal

---
+++ js-tip-calculator/meal.js
@@ -0,0 +1,8 @@
+var mealCost = 56;
+const tipPercentage = 10;
+
+var tipAmount = (mealCost * 10) / 100;
+
+var totalCost = mealCost + tipAmount;
+
+console.log("Total cost of bill is " + totalCost + " euros.");
