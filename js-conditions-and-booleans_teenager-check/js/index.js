ae52ef336bb7289ee7e75eff59ee5453b6980130
emyjamalian <emy.jamalian@gmail.com>
2023-11-06 20:57:58 +0100

    update

--- js-conditions-and-booleans_teenager-check/js/index.js
+++ js-conditions-and-booleans_teenager-check/js/index.js
@@ -5,9 +5,7 @@ const output = document.querySelector('[data-js="age-output"]');
 button.addEventListener("click", () => {
   // Number(x) converts the string from the input to a number
   // Beware: input values are *always* strings!
-  let age = Number(input.value);
-  //age = parseInt(age, 10);
-  console.log(typeof age);
+  const age = Number(input.value);
 
   // Exercise:
   // Use conditions and the logical AND operator to write
