d51f3b6a2ecc99d2ce30da808f4efae4d74bb0b3
emyjamalian <emy.jamalian@gmail.com>
2023-11-07 10:57:17 +0100

    update files

--- js-tip-calculator/meal.js
+++
@@ -1,8 +0,0 @@
-let mealCost = 56;
-const tipPercentage = 10;
-
-let tipAmount = (mealCost * 10) / 100;
-
-let totalCost = mealCost + tipAmount;
-
-console.log("Total cost of bill is: " + totalCost + " euros.");
