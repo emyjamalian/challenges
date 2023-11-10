c46f366768c68df08586d7c937a128658a3bb0dc
emyjamalian <emy.jamalian@gmail.com>
2023-11-10 11:40:02 +0100

    handle form submit

--- js/js-forms_handle-form-submit/js/index.js
+++ js/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+  event.target.reset();
+  event.target.focus(data.firstname);
+
+  const age = Number(data.age);
+  const badAss = Number(data.badness);
+  console.log(age);
+  console.log(badAss);
+  const sum = age + badAss;
+
+  console.log(sum);
+});
