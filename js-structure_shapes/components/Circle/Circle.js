3bb568c8ff7a83fca1b4a0f691fa1ab441131635
emyjamalian <emy.jamalian@gmail.com>
2023-11-17 11:32:32 +0100

    refactor shapes

---
+++ js-structure_shapes/components/Circle/Circle.js
@@ -0,0 +1,9 @@
+import { getRandomColor } from "./utils/randomColor.js";
+
+const circle = document.createElement("div");
+circle.classList.add("circle");
+circle.addEventListener("click", () => {
+  circle.style.backgroundColor = getRandomColor();
+});
+
+export { circle };
