372d1aa5b812895e877c1da6e7bf31ad7f8904a1
emyjamalian <emy.jamalian@gmail.com>
2023-11-17 13:52:10 +0100

    refactor quiz app

---
+++ js-structure_quiz-app/components/App/App.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header.js";
+import { Form } from "../components/Form/Form.js";
+import { CardList } from "../components/CardList/CardList.js";
+
+function App() {
+  const app = document.createElement("main");
+  app.classList.add("app");
+  app.append(Header(), Form(), CardList());
+
+  return app;
+}
+
+export { App };
