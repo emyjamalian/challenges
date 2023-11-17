b87641f610c6cf01b69ee91b2a8401c324529dda
emyjamalian <emy.jamalian@gmail.com>
2023-11-17 13:52:10 +0100

    refactor quiz app

---
+++ js-structure_quiz-app/components/CardList/CardList.js
@@ -0,0 +1,15 @@
+import { Card, cards } from "../components/Card/Card.js";
+
+function CardList() {
+  const cardList = document.createElement("section");
+  cardList.classList.add("card-list");
+
+  cards.forEach((card) => {
+    const cardElement = Card(card);
+    cardList.append(cardElement);
+  });
+
+  return cardList;
+}
+
+export { CardList };
