02f51f0803d9617ff22cd854f53de1433e3f9ef9
emyjamalian <emy.jamalian@gmail.com>
2023-11-17 11:32:32 +0100

    refactor shapes

---
+++ js-structure_shapes/components/Square/Square.js
@@ -0,0 +1,9 @@
+import { getRandomColor } from "./utils/randomColor.js";
+
+const square = document.createElement("div");
+square.classList.add("square");
+square.addEventListener("click", () => {
+  square.style.backgroundColor = getRandomColor();
+});
+
+export { square };
--- js-structure_shapes/index.js
+++ js-structure_shapes/index.js
@@ -1,23 +1,7 @@
-import { getRandomColor } from "./utils/randomColor.js";
+import circle from "./components/Circle/Circle.js";
+import square from "./components/Pentagon/Pentagon.js";
+import pentagon from "./components/Square/Square.js";
 
 const root = document.getElementById("root");
-
-const circle = document.createElement("div");
-circle.classList.add("circle");
-circle.addEventListener("click", () => {
-  circle.style.backgroundColor = getRandomColor();
-});
-
-const square = document.createElement("div");
-square.classList.add("square");
-square.addEventListener("click", () => {
-  square.style.backgroundColor = getRandomColor();
-});
-
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = getRandomColor();
-});
 
 root.append(circle, square, pentagon);
