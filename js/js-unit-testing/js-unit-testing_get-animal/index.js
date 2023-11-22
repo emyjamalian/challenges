81d91cd2cf5a5c24254b60c8fe732f76dcfe8fb1
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2023-11-22 10:40:33 +0100

    get animal

--- js/js-unit-testing/js-unit-testing_get-animal/index.js
+++ js/js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,5 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (animal === "cats") return `I totally love ${animal}!`;
+  if (typeof animal === "string") return `I like ${animal}!`;
+  else return `I do not like animals at all!`;
+}
