32df65ce8f858f7507ecfbf896925363502540b5
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2023-11-27 16:35:30 +0100

    sum challenge update

--- react/react-props_sum/src/App.js
+++ react/react-props_sum/src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <Sum valueA="3" valueB="4" />;
+  return <Sum valueA={3} valueB={4} />;
 }
 
 function Sum({ valueA, valueB }) {
-  return <h1>{Number(valueA) + Number(valueB)}</h1>;
+  return <h1>{valueA + valueB}</h1>;
 }
