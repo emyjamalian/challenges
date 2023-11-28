b1cfe0ec94a1d73965b118ff14c54aadb936cceb
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2023-11-28 10:46:04 +0100

    nested buttons challenge

--- react/react-nesting_buttons-and-children/src/App.js
+++ react/react-nesting_buttons-and-children/src/App.js
@@ -4,18 +4,19 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>{"I"}</Button>
+      <Button>{"am"}</Button>
+      <Button>{"very"}</Button>
+      <Button>{"grumpy"}</Button>
+      <Button>{"today!"}</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
