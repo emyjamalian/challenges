9470542eb64724d98c5082ddc0c3f6c7e6774978
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2023-11-29 16:06:36 +0100

    array users challenge

--- react/react-with-arrays_users/src/App.js
+++ react/react-with-arrays_users/src/App.js
@@ -9,7 +9,16 @@ export default function App() {
     <main className="app">
       <Title text="ClientBoard" />
       <div className="app__card-grid">
-        <Card user={USERS[0]} />
+        {USERS.map((user) => {
+          return (
+            <Card
+              key={user.id}
+              name={user.name}
+              roles={user.roles}
+              about={user.about}
+            />
+          );
+        })}
       </div>
     </main>
   );
