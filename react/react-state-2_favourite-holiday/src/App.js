f4307432d506a8121561d7126a5bbdb9aae38c86
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2023-11-30 14:58:45 +0100

    holiday challenge

--- react/react-state-2_favourite-holiday/src/App.js
+++ react/react-state-2_favourite-holiday/src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./styles.css";
 
 export default function App() {
+  const [formValues, setFormValues] = useState({
+    holiday: "",
+    date: "",
+  });
+
   function handleSubmit(event) {
     event.preventDefault();
+    let holidayValue = event.target.elements.holiday.value;
+    const dateValue = event.target.elements.date.value;
+    setFormValues({
+      holiday: holidayValue,
+      date: dateValue,
+    });
+
+    //reset form
+    event.target.reset();
+    event.target.elements.holiday.focus();
   }
 
   return (
@@ -24,16 +40,20 @@ export default function App() {
         />
         <label htmlFor="date">Date: </label>
         <input id="date" type="date" name="date" />
-        <button type="submit" className="form__submit-button">
+        <button
+          type="submit"
+          className="form__submit-button"
+          onClick={() => setFormValues({ holiday: "", date: "" })}
+        >
           Submit
         </button>
       </form>
       <h2>Output of Submitted Data</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{formValues.holiday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{formValues.date}</span>
       </p>
     </div>
   );
