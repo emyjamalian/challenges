abfeb3081aee1ac51997c25e064f4c00e72cce6c
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2023-12-04 12:15:28 +0100

    dice challenge

--- react/react-with-local-storage_dice/src/App.js
+++ react/react-with-local-storage_dice/src/App.js
@@ -1,15 +1,28 @@
 import { D6Button } from "./components/D6Button";
+import { getD6Roll } from "./utils";
+
 import History from "./components/History";
 import "./styles.css";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("rolls", { defaultValue: [] });
+
+  const handleRoll = () => {
+    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  };
+
+  console.log(rolls);
+  const currentRollValue = rolls[0]?.value;
+  console.log(currentRollValue);
+
   return (
     <div className="app">
       <main className="app__main">
-        <D6Button />
+        <D6Button value={currentRollValue} onRoll={handleRoll} />
       </main>
       <aside className="app__aside">
-        <History rolls={[]} />
+        <History rolls={rolls} />
       </aside>
     </div>
   );
