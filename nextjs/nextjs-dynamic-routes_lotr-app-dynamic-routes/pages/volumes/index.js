db1f042f9ee597ddfffd552f8173fe13a4382883
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2023-12-07 12:11:15 +0100

    random button

--- nextjs/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
+++ nextjs/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -1,11 +1,23 @@
 import Link from "next/link";
-import { introduction } from "../../lib/data";
+import { volumes } from "../../lib/data.js";
+import { useRouter } from "next/router";
 
 export default function Volumes() {
+  const router = useRouter();
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function handleRandomVolume() {
+    const randomVolume = getRandomElement(volumes);
+    console.log(randomVolume);
+    return router.push(`/volumes/${randomVolume.slug}`);
+  }
   return (
     <>
       <h1>The Lord of the Rings</h1>
-      <p>{introduction}</p>
+      <p>{volumes.introduction}</p>
       <ul>
         <li>
           <Link href="/volumes/the-fellowship-of-the-ring">
@@ -21,6 +33,7 @@ export default function Volumes() {
           </Link>
         </li>
       </ul>
+      <button onClick={handleRandomVolume}>Random Volume</button>
     </>
   );
 }
