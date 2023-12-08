63a1466503e895d80c7a54b80350478111ff1d3d
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2023-12-08 10:59:04 +0100

    challenge test

--- nextjs/react-component-testing_scorekeeper/components/Button/index.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export default styled.button`
-  background-color: lightgray;
-  border: 1px solid black;
-  width: 100%;
-`;
--- nextjs/react-component-testing_scorekeeper/components/Player/Player.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import Player from ".";
-
-test("renders player information and two buttons", () => {});
-
-test("calls callbacks when increasing or decreasing score", async () => {});
