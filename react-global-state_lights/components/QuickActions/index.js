c02c500e690a7974fc2b5281e468cbb1099ff0c7
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2023-12-11 15:32:58 +0100

    global state light challlenge

--- react-global-state_lights/components/QuickActions/index.js
+++ react-global-state_lights/components/QuickActions/index.js
@@ -7,22 +7,25 @@ const StyledQuickActions = styled.div`
   gap: 16px;
 `;
 
-export default function QuickActions() {
+export default function QuickActions({
+  allLightsOff,
+  allLightsOn,
+  lightsOnCount,
+  lights,
+}) {
   return (
     <StyledQuickActions>
       <Button
         type="button"
-        onClick={() => {
-          console.log("Turn all lights off");
-        }}
+        onClick={allLightsOff}
+        disabled={lightsOnCount === 0}
       >
         Turn all lights off
       </Button>
       <Button
         type="button"
-        onClick={() => {
-          console.log("Turn all lights on");
-        }}
+        onClick={allLightsOn}
+        disabled={lightsOnCount === lights.length}
       >
         Turn all lights on
       </Button>
--- react-global-state_lights/pages/actions.js
+++ react-global-state_lights/pages/actions.js
@@ -1,12 +1,26 @@
 import Link from "../components/Link";
 import QuickActions from "../components/QuickActions";
 
-export default function Actions() {
+export default function Actions(
+  lights,
+  allLightsOn,
+  allLightsOff,
+  lightsOnCount
+) {
   return (
     <>
       <Link href="/">← Back home</Link>
       <h1>Quick Actions</h1>
-      <QuickActions />
+      <QuickActions
+        lights={lights}
+        lightsOnCount={lightsOnCount}
+        allLightsOn={() => {
+          allLightsOn();
+        }}
+        allLightsOff={() => {
+          allLightsOff();
+        }}
+      />
     </>
   );
 }
