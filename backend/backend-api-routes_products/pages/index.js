5c39de44ede0cd955fcbec1329899cbd9964d895
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2024-01-09 10:28:42 +0100

    api challenge

--- backend/backend-api-routes_products/pages/index.js
+++ backend/backend-api-routes_products/pages/index.js
@@ -1,7 +1,14 @@
+import { SWRConfig } from "swr";
+
 export default function HomePage() {
+  const fetcher = (url) => fetch(url).then((response) => response.json());
+
   return (
     <div>
       <h1>Welcome to Next.js API Routes!</h1>
+      <SWRConfig value={{ fetcher }}>
+        <Component {...pageProps} />
+      </SWRConfig>
     </div>
   );
 }
