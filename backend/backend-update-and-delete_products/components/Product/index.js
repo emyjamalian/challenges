9113c285b8c7c00bd6daa13399f1c9cc4470fcf9
Elmira Jamalian <51411715+emyjamalian@users.noreply.github.com>
2024-01-10 11:51:26 +0100

    edit product

--- backend/backend-update-and-delete_products/components/Product/index.js
+++ backend/backend-update-and-delete_products/components/Product/index.js
@@ -1,14 +1,48 @@
 import useSWR from "swr";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
 import Comments from "../Comments";
+import ProductForm from "../ProductForm";
 
 export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
   const router = useRouter();
   const { id } = router.query;
 
   const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { mutate } = useSWR("/api/products");
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
+
+  async function handleDeleteProduct(id) {
+    const response = await fetch(`/api/products/${id}`, { method: "DELETE" });
+
+    if (!response.ok) {
+      console.log(response.status);
+      return;
+    }
+
+    router.push("/");
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -25,6 +59,27 @@ export default function Product() {
       <p>
         Price: {data.price} {data.currency}
       </p>
+      <button
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        <span role="img" aria-label="A pencil">
+          ✏️
+        </span>
+      </button>
+      <button onClick={() => handleDeleteProduct(id)} disabled={isEditMode}>
+        <span role="img" aria-label="A cross indicating deletion">
+          ❌
+        </span>
+      </button>
+      {isEditMode && (
+        <ProductForm
+          onSubmit={handleEditProduct}
+          value={data.product}
+          isEditMode={true}
+        />
+      )}
       {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
--- backend/backend-update-and-delete_products/components/ProductForm/index.js
+++ backend/backend-update-and-delete_products/components/ProductForm/index.js
@@ -1,50 +1,24 @@
 import { StyledForm, StyledHeading, StyledLabel } from "./ProductForm.styled";
 import { StyledButton } from "../Button/Button.styled";
-import useSWR from "swr";
 
-export default function ProductForm() {
-  const { mutate } = useSWR("/api/products");
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const productData = Object.fromEntries(formData);
-
-    const response = await fetch("/api/products", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(productData),
-    });
-
-    if (!response.ok) {
-      console.error(response.status);
-      return;
-    }
-
-    mutate();
-    event.target.reset();
-  }
-
+export default function ProductForm({ onSubmit, isEditMode }) {
   return (
-    <StyledForm onSubmit={handleSubmit}>
+    <StyledForm onSubmit={onSubmit}>
       <StyledHeading>Add a new Fish</StyledHeading>
       <StyledLabel htmlFor="name">
-        Name:
+        {isEditMode ? "Edit the product name" : "Name"}
         <input type="text" id="name" name="name" />
       </StyledLabel>
       <StyledLabel htmlFor="description">
-        Description:
+        {isEditMode ? "Edit the product description" : "Description"}
         <input type="text" id="description" name="description" />
       </StyledLabel>
       <StyledLabel htmlFor="price">
-        Price:
+        {isEditMode ? "Edit the product price" : "Price"}
         <input type="number" id="price" name="price" min="0" />
       </StyledLabel>
       <StyledLabel htmlFor="currency">
-        Currency:
+        {isEditMode ? "Edit the product currency" : "Select a currency"}
         <select id="currency" name="currency">
           <option value="EUR">EUR</option>
           <option value="USD">USD</option>
